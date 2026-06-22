4e36c3b06829521b3f4f4397292d73a6452056ea
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-06-23 05:25:06 +0630

    Fix dependency conflict and remove lockfile

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <div className="mb-2">
                <UserCount />
             </div>
-            © 2026 KP VPN WEB • အားလုံးကို ချစ်ခင်စွာဖြင့်
+            © 2026 KP VPN WEB • အားလုံးကို ချစ်ခင်စွာဖြင့် 💕
           </footer>
         </ClientWrapper>
       </body>
