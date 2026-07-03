df4f180fa6d98997b0841b09247cb02355e2a186
KP-CHANNEL-KP <kopaing232003@gmail.com>
2026-07-03 09:37:45 +0630

    Add bot to web project

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config: { externals: string[]; externalsType: string; }, { isServer }: any) => {
     if (isServer) {
       // cloudflare:sockets ကို webpack ကနေ bundle မလုပ်ဘဲ external
       // အနေနဲ့ ထားရမယ် — externalsType: 'commonjs' ကို ထပ်ထည့်ရမှ
